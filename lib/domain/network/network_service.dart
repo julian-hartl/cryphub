047159b166158c225cb2f5e3173d58a524e11cd1
JuHar-dev <julian.hartl.dev@gmail.com>
2022-01-17 23:04:52 +0100

    Added comments to interfaces to better explain their usage

--- lib/domain/network/network_service.dart
+++ lib/domain/network/network_service.dart
@@ -1,6 +1,13 @@
+import 'package:cryphub/domain/network/network_timeout_exception.dart';
+
 import 'network_response.dart';
 
 abstract class INetworkService {
+  /// Makes a get request to [url]
+  /// [headers] are sent to server
+  /// [queryParameters] are added to the end of the url
+  /// Returns a [NetworkResponse].
+  /// Throws a [NetworkTimeoutException] when either requested server does not receive the request or does not respond.
   Future<NetworkResponse> get(
     String url, {
     Map<String, dynamic>? headers,
--- lib/domain/settings/settings_repository.dart
+++ lib/domain/settings/settings_repository.dart
@@ -1,6 +1,15 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'settings.dart';
 
 abstract class ISettingsRepository {
+  /// Replaces the current settings with the new [settings].
   Future<void> updateSettings(Settings settings);
+
+  /// Get the current settings.
+  /// Make sure to initialize [SharedPreferences] before:
+  /// ```dart
+  /// final sp = await SharedPreferences.getInstance();
+  /// ```
   Settings readSettings();
 }
