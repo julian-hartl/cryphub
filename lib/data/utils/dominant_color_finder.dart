9054b3d557630c038f90756ad8fbc15dd8f149a8
JuHar-dev <julian.hartl.dev@gmail.com>
2022-01-18 11:20:41 +0100

    Added comment to dominant_color_finder.dart

--- lib/data/utils/dominant_color_finder.dart
+++ lib/data/utils/dominant_color_finder.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:palette_generator/palette_generator.dart';
 
-// https://stackoverflow.com/questions/62718295/how-to-get-dominant-color-from-image-in-flutter
 @lazySingleton
 class DominantColorFinder {
+  /// Finds the dominant color from the provided [image].
+  ///
+  /// Dominant color is the color that appears the most in the image.
+  ///
+  /// See https://stackoverflow.com/questions/62718295/how-to-get-dominant-color-from-image-in-flutter
   Future<Color> find(ImageProvider image) async {
     final paletteGenerator =
         await PaletteGenerator.fromImageProvider(image, maximumColorCount: 1);
